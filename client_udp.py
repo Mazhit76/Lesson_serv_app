31ed162a28a5e38dc7e2228a631266c09b42740d
Mazhit76 <62520216+Mazhit76@users.noreply.github.com>
2021-02-26 20:56:09 +0400

    1

--- client_udp.py
+++
@@ -1,8 +0,0 @@
-# Программа клиента, передающего серверу сообщения при каждом запросе на соединение
-from socket import *
-
-with socket(AF_INET, SOCK_DGRAM) as s:        # Определяем UDP-протокол
-    s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-    while True:
-        msg = 'Запрос на соединение!'
-        s.sendto(msg.encode('utf-8'), ("127.0.0.1", 8888))
